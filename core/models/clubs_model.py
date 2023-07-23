4fde9f69dfe753e02cc8a2e0abe8b15ec38d3ac1
rusia9217 <110519493+unnimm@users.noreply.github.com>
2023-07-23 14:47:35 +0900

    수정 사항 3차
    image_urls, content-> main_content. subcontent로 나눴음,

--- core/models/clubs_model.py
+++ core/models/clubs_model.py
@@ -2,10 +2,11 @@ from pydantic import BaseModel
 
 class Club(BaseModel):
 	title: str
-	content: str
+	main_content: str
+	sub_content: str
 	author: str
 	user_id: str
-	image_url: list[str]
+	image_urls: list[str]
 	activity_tags: list[str]
 	tag1: str
 	tag2: str
